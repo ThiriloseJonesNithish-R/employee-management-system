e3c3febc8f11a97fe9163d42aa929ded38d972d4
ThiriloseJonesNithish-R <mj360594@gmail.com>
2026-03-10 16:22:42 +0530

    Default controller added

---
+++ src/main/java/com/wipro/cpc/controller/DefaultController.java
@@ -0,0 +1,15 @@
+package com.wipro.cpc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DefaultController {
+
+    @GetMapping
+    public String home() {
+        return "API is running. No UI available.";
+    }
+}
